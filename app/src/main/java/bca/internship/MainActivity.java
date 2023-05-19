0d5430d5848d575f9435e57126145797d15f7e92
Sagar Khatri <khatrisagar2@gmail.com>
2023-05-19 14:32:03 +0530

    MainActivity.java Changes

--- app/src/main/java/bca/internship/MainActivity.java
+++ app/src/main/java/bca/internship/MainActivity.java
@@ -1,14 +1,63 @@
 package bca.internship;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText email,password;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.main_login);
+        email = findViewById(R.id.main_email);
+        password = findViewById(R.id.main_password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Character Password Required");
+                }
+                else {
+                    if(email.getText().toString().trim().equals("admin@gmail.com") && password.getText().toString().trim().equalsIgnoreCase("Admin@123")){
+                        System.out.println("Login Successfully");
+                        Log.d("LOGIN", "Login Successfully");
+                        Log.e("LOGIN", "Login Successfully");
+
+                        Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                        Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_LONG).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Login Unsuccessfully", Toast.LENGTH_LONG).show();
+                        Snackbar.make(view, "Login Unsuccessfully", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
     }
 }
