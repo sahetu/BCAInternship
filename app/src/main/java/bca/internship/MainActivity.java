bfcaff93d543e26237a50346e296480902aec749
Sagar Khatri <khatrisagar2@gmail.com>
2023-05-22 14:43:38 +0530

    22 May Changes

--- app/src/main/java/bca/internship/MainActivity.java
+++ app/src/main/java/bca/internship/MainActivity.java
@@ -1,10 +1,12 @@
 package bca.internship;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +16,11 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
 
     Button login;
-    EditText email,password;
+    EditText email, password;
 
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    TextView createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,32 +34,46 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(email.getText().toString().trim().equals("")){
+                if (email.getText().toString().trim().equals("")) {
                     email.setError("Email Id Required");
-                }
-                else if(!email.getText().toString().trim().matches(emailPattern)){
+                } else if (!email.getText().toString().trim().matches(emailPattern)) {
                     email.setError("Valid Email Id Required");
-                }
-                else if(password.getText().toString().trim().equals("")){
+                } else if (password.getText().toString().trim().equals("")) {
                     password.setError("Password Required");
-                }
-                else if(password.getText().toString().trim().length()<6){
+                } else if (password.getText().toString().trim().length() < 6) {
                     password.setError("Min. 6 Character Password Required");
-                }
-                else {
-                    if(email.getText().toString().trim().equals("admin@gmail.com") && password.getText().toString().trim().equalsIgnoreCase("Admin@123")){
+                } else {
+                    if (email.getText().toString().trim().equals("admin@gmail.com") && password.getText().toString().trim().equalsIgnoreCase("Admin@123")) {
                         System.out.println("Login Successfully");
                         Log.d("LOGIN", "Login Successfully");
                         Log.e("LOGIN", "Login Successfully");
 
-                        Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
-                        Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_LONG).show();
-                    }
-                    else{
-                        Toast.makeText(MainActivity.this, "Login Unsuccessfully", Toast.LENGTH_LONG).show();
-                        Snackbar.make(view, "Login Unsuccessfully", Snackbar.LENGTH_LONG).show();
+                        //Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                        new CommonMethod(MainActivity.this, "Login Successfully");
+                        new CommonMethod(view, "Login Successfully");
+                        //Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_LONG).show();
+
+                        /*Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);*/
+                        new CommonMethod(MainActivity.this, HomeActivity.class);
+
+                    } else {
+                        //Toast.makeText(MainActivity.this, "Login Unsuccessfully", Toast.LENGTH_LONG).show();
+                        new CommonMethod(MainActivity.this, "Login Unsuccessfully");
+                        //Snackbar.make(view, "Login Unsuccessfully", Snackbar.LENGTH_LONG).show();
+                        new CommonMethod(view, "Login Unsuccessfully");
                     }
                 }
+            }
+        });
+
+        createAccount = findViewById(R.id.main_create_account);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Intent intent = new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(intent);*/
+                new CommonMethod(MainActivity.this, SignupActivity.class);
             }
         });
 
